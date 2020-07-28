1af0b201a78df9a792589a45cc199b8a69c21e83
kumble-07 <sidkumble@gmail.com>
2020-07-29 00:37:23 +0530

    ionic

---
+++ contact.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EmailComposer } from '@ionic-native/email-composer';
+import { CallNumber } from '@ionic-native/call-number';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html',
+})
+export class ContactPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  private emailComposer: EmailComposer,
+  private callNumber: CallNumber) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ContactPage');
+  }
+
+  sendEmail() {
+    let email = {
+      to: 'confusion@food.net',
+      subject: '[ConFusion]: Query',
+      body: 'Dear Sir/Madam:',
+      isHtml: true
+    };
+    this.emailComposer.open(email);
+  }
+  callRestaurent() {
+    this.callNumber.callNumber('+85212345678', false)
+      .then(res => console.log("Launched dialer!", res))
+      .catch(err => console.log("Error launching dialer", err));
+  }
+}
